9f4c6a7731151c625788d6a2b6b9b810e6900a2a
Royal <aeo_royals@outlook.com>
2023-06-20 14:30:20 +0530

    Video Scraper

--- WScr.py
+++ WScr.py
@@ -4,22 +4,70 @@ import os
 import sys
 import pathlib
 
+
+def getwebside(link):
+    return bs4.BeautifulSoup(requests.get(link).text, features="html.parser")
+
+
 def scrapeImgsLiks(link):
-    website = requests.get(link).text
-    website = bs4.BeautifulSoup(website,features="html.parser")
+    website = getwebside(link)
     ImgLi = list(website.find_all("img"))
     OnlyLinks = []
     for linkI in ImgLi:
         linkI = str(linkI)
-        chunks = linkI.split(" ") # split between diff attributes.
+        chunks = linkI.split(" ")  # split between diff attributes.
         for chunk in chunks:
             if chunk.startswith('src="'):     # check if it's the src link.
-                chunk = chunk.replace('src="',"")         # removing src=".
-                chunk = chunk.replace('"',"")             # removing back quote.
+                chunk = chunk.replace('src="', "")         # removing src=".
+                # removing back quote.
+                chunk = chunk.replace('"', "")
                 OnlyLinks.append(chunk)
                 break
     return OnlyLinks
-def delStr(str1:str,str2:str):
+
+
+def scrapeVidLinks(link):
+    webside = getwebside(link)
+    VidLi = list(webside.find_all("video"))
+    OnlyLinks = []
+    for linkI in VidLi:
+        linkI = str(linkI)
+        if "src=" in linkI:
+            linkI = str(linkI)
+            chunks = linkI.split(" ")  # split between diff attributes.
+            for chunk in chunks:
+                if chunk.startswith('src="'):     # check if it's the src link.
+                    # removing src=".
+                    chunk = chunk.replace('src="', "")
+                    # removing back quote.
+                    chunk = chunk.replace('"', "")
+                    chunk = chunk[:chunk.index(">")] # if its the last attribute of the tag
+                    if "https:" not in chunk:
+                        chunk = "https:".__add__(chunk)
+                    OnlyLinks.append(chunk)
+                    break
+        else:
+            tagsoup = bs4.BeautifulSoup(linkI, features="html.parser").find_all("source")
+
+            for spoon in tagsoup:
+                chunks = str(spoon).split(" ")
+                for chunk in chunks:
+                    # check if it's the src link.
+                    if chunk.startswith('src="'):
+                        # removing src=".
+                        chunk = chunk.replace('src="', "")
+                        # removing back quote.
+                        chunk = chunk.replace('"', "")
+                        chunk = chunk[:chunk.index(">")] # if its the last attribute of the tag
+                        if "https:" not in chunk:
+                            chunk = "https:".__add__(chunk)
+                        OnlyLinks.append(chunk)
+                        break
+
+    return OnlyLinks
+
+
+def delStr(str1: str, str2: str):
     cou = len(str1)
     if len(str1) > len(str2):
         con = len(str2)
@@ -31,7 +79,7 @@ def delStr(str1:str,str2:str):
                 pass
             p = i
         return str1[p+1:]
-        
+
     elif len(str2) > len(str1):
         con = len(str1)
         p = 0
@@ -43,10 +91,12 @@ def delStr(str1:str,str2:str):
             p = i
         return str2[p+1:]
 
-def SetDirs(Path:str):
-    initP = pathlib.Path().absolute().as_uri().replace("file:///","")
-    Path = pathlib.Path(Path).absolute().as_uri().replace("file:///","")
-    Path = delStr(Path,initP)
+
+def SetDirs(Path: str):
+  if (Path != ".") or (Path == ""):
+    initP = pathlib.Path().absolute().as_uri().replace("file:///", "")
+    Path = pathlib.Path(Path).absolute().as_uri().replace("file:///", "")
+    Path = delStr(Path, initP)
     if "//" in Path:
         DirLi = Path.split("//")
     elif "\\" in Path:
@@ -55,36 +105,48 @@ def SetDirs(Path:str):
         DirLi = [Path[1:]]
     for i in DirLi:
         try:
-          os.mkdir(i)
-          os.chdir(i)
+            os.mkdir(i)
+            os.chdir(i)
         except:
             os.chdir(i)
     os.chdir(initP)
-    
+  else:
+      pass
 
-def findExt(link):
-    ext = link.split(".")[-1]
-    if len(ext) > 5:
-        return None
-    else:
-        return ext
+def saveVids(ScrabedLList: list, targetLoc=".", format: str = "mp4"):
+    con = 0
+    SetDirs(targetLoc)
+    print("Path set! \n\n")
+    for link in ScrabedLList:
+        vid = requests.get(link).content
+        if sys.getsizeof(vid) < 50:
+            print(
+                f"Scraping Failed! | Image Name: {con}.{format} | size: {sys.getsizeof(vid)} | from: {link}\n")
+            continue
+        print(
+            f"Scraping Successful! | Image Name: {con}.{format} | size: {sys.getsizeof(vid)} | from: {link}\n")
+        with open(f"{targetLoc}//{con}.{format}", "xb") as f:
+            f.write(vid)
+            con += 1
 
-def saveImgs(ScrabedLList:list,targetLoc=".",format:str="jpg"):
+def saveImgs(ScrabedLList: list, targetLoc=".", format: str = "jpg"):
     con = 0
     SetDirs(targetLoc)
     print("Path set! \n\n")
     for link in ScrabedLList:
         photo = requests.get(link).content
         if sys.getsizeof(photo) < 50:
-            print(f"Scraping Failed! | Image Name: {con}.{format} | size: {sys.getsizeof(photo)} | from: {link}\n")
+            print(
+                f"Scraping Failed! | Image Name: {con}.{format} | size: {sys.getsizeof(photo)} | from: {link}\n")
             continue
-        print(f"Scraping Successful! | Image Name: {con}.{format} | size: {sys.getsizeof(photo)} | from: {link}\n")
-        with open(f"{targetLoc}//{con}.{format}","xb") as f:
-               f.write(photo)
-               con += 1
-        
+        print(
+            f"Scraping Successful! | Image Name: {con}.{format} | size: {sys.getsizeof(photo)} | from: {link}\n")
+        with open(f"{targetLoc}//{con}.{format}", "xb") as f:
+            f.write(photo)
+            con += 1
+
 
 if __name__ == "__main__":
-    li = scrapeImgsLiks("https://unsplash.com/s/photos/gallery")
-    saveImgs(li,format="avif",targetLoc=".//Newfol//Amrut")
-    pass
+    i = scrapeVidLinks("https://samplelib.com/sample-mp4.html")
+    saveVids(i,format="mp4")
+    
